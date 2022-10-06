7534f31c325b2d016ad0b8db3d78c68d589dbd98
leyulin <linleyu111@gmail.com>
2022-10-07 03:24:34 +0800

    Site updated: 2022-10-07 03:24:33

--- js/time.js
+++ js/time.js
@@ -1,13 +1,7 @@
-$(document).ready(function() {
-    var myDate = new Date;
-    var year = myDate.getFullYear(); //获取当前年
-    var mon = myDate.getMonth() + 1; //获取当前月
-    var date = myDate.getDate(); //获取当前日
-    var h = myDate.getHours();//获取当前小时数(0-23)
-    var m = myDate.getMinutes();//获取当前分钟数(0-59)
-    var s = myDate.getSeconds();//获取当前秒
-    var week = myDate.getDay();
-    var weeks = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"];
-    console.log(year, mon, date, weeks[week])
-    $("#time").html("今天是"+ year + "年" + mon + "月" + date + "日" + weeks[week] + h + "时" + m + "分" + s +"秒");
-})
+var nowTime ;
+function play(){
+        var time = new Date();
+        nowTime = "北京时间:"+time.getFullYear()+"年"+time.getMonth()+"月"+time.getDate()+"日"+time.getHours()+"时"+time.getMinutes()+"分"+time.getSeconds()+"秒";
+        document.getElementById("time").innerHTML = nowTime;
+}
+setInterval(play,1000);//每一秒执行一次
