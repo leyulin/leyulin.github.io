cc4dcda935a80f804656634eff485e6cf390f544
leyulin <linleyu111@gmail.com>
2022-10-23 15:18:32 +0800

    Site updated: 2022-10-23 15:18:31

--- js/copy.js
+++
@@ -1,9 +0,0 @@
-function copy(val) {
-        var clipboard = new ClipboardJS(val);
-        clipboard.on('success', function(e) {
-                swal({ button: "确定",title: "歌名:"+ val.innerText +" 复制成功！" ,icon:"success",text:"记得给菜菜打米嗷！"});
-        });
-        clipboard.on('error', function(e) {
-                swal("复制失败 请联系缸子的奶爸");
-        });
-}
--- js/time.js
+++
@@ -1,7 +0,0 @@
-var nowTime ;
-function play(){
-        var time = new Date();
-        nowTime = "北京时间:"+time.getFullYear()+"年"+time.getMonth()+"月"+time.getDate()+"日"+time.getHours()+"时"+time.getMinutes()+"分"+time.getSeconds()+"秒";
-        document.getElementById("time").innerHTML = nowTime;
-}
-setInterval(play,1000);//每一秒执行一次
